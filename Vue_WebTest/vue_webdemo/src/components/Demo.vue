3238f3679ea2659ee36184560e9bf5a4a0b8cfc0
lpp3357 <18398463357@163.com>
2019-05-29 11:40:09 +0800

    1

---
+++ Vue_WebTest/vue_webdemo/src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">{{message}}</div>
+</template>
+<script>
+import Vue from "vue";
+export default {
+  name: "Demo",
+  data() {
+    //message: "";
+    return {
+      message: ""
+    };
+  }
+};
+var app = new Vue({
+  el: "#app",
+  data: { message: "Hello Demo" }
+});
+app.message = "111";
+</script>
--- Vue_WebTest/vue_webdemo/src/router/index.js
+++ Vue_WebTest/vue_webdemo/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Demo from '@/components/Demo'
+import Demo01 from '@/components/demoHtml/Demo01'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
+    // {
+    //   path: '/',
+    //   name: 'Demo',
+    //   component: Demo
+    // }
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Demo01',
+      component: Demo01
     }
   ]
 })
